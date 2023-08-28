6abb4410304cdcb2884c60c341e7f753bdca5f52
Chepkosgei-Anastansia <anastansiachepkosgei@gmail.com>
2023-08-28 22:56:33 +0300

    Simple react tutorial

---
+++ src/Table.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+const TableHeader = ()=> {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+
+}
+
+// Simple Component
+const TableBody = () => {
+    return (
+        <tbody />       
+    )
+}
+
+class Table extends Component {
+    render() {
+        const { characterData } = this.props
+        return(
+           <table>
+            <TableHeader />
+            <TableBody characterData={characterData} />
+           </table>
+
+        )
+    }
+
+}
+
+export default Table
